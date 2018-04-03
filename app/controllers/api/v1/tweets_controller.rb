f4e6af85a0e274414570461d9c2ab7fc5937c02a
renanz <renan_zelaya@unitec.edu>
2018-04-02 22:29:56 -0600

    added update

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -1,8 +1,24 @@
 module Api::V1
     class TweetsController < ApplicationController
         def index
-            @tweets = Tweet.all
+            @tweets = Tweet.order("created_at DESC")
             render json: @tweets
         end
+
+        def create 
+            @tweet = Tweet.create(tweets_params)
+            render json: @tweet
+        end
+
+        def update
+            @tweet = Tweet.find(params[:id])
+            @tweet.update_attributes(tweets_params)
+            render json: @tweet
+          end
+
+        private
+            def tweets_params
+                params.require(:tweet).permit(:username, :body)
+            end
     end
 end
