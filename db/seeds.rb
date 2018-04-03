cc448b03adf7c297d0596cc31272740d19b62413
renanz <renan_zelaya@unitec.edu>
2018-04-02 20:50:19 -0600

    added model Tweet and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+tweets = Tweet.create(
+  [
+    {
+      username: "renanz",
+      body: "first tweet"
+    },
+    {
+      username: "joh",
+      body: "presidente "
+    },
+    {
+      username: "renanz",
+      body: "seed examen"
+    }
+  ])
